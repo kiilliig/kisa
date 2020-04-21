8d29def391a6eb32a8ebcdaff182f6af101162e0
kiilliig <leema0712@gmail.com>
2020-04-21 12:49:26 +0900

    commit2

---
+++ day2/function.js
@@ -0,0 +1,20 @@
+function plus(p1, p2){
+  return p1+p2;
+}
+
+function minus(p1, p2){
+  return p1-p2;
+}
+
+function multi(p1, p2){
+  return p1*p2;
+}
+
+function div(p1, p2){
+  return p1/p2;
+}
+
+console.log(plus(4,2))
+console.log(minus(4,2))
+console.log(multi(4,2))
+console.log(div(4,2))
