df52238ab9272af6a32e495aaa8fe6c83230bd83
kiilliig <leema0712@gmail.com>
2020-04-21 12:44:25 +0900

    commit1

---
+++ day2/object.js
@@ -0,0 +1,19 @@
+///car 라는 오브젝트 만들기
+var car = {
+	name : "sonata",
+	ph : "500ph",
+	start : function () {
+		console.log("engine is starting");
+	},
+	stop : function () {
+		console.log("engine is stoped");
+	}
+}
+
+var person = {
+    name : "gwanwoo",
+    age : 32
+}
+
+console.log(car.name);
+car.start();
